ceaa1900875eb0242a8b547d7897d879fe19b41f
jrmensah <jerilynmensah@gmail.com>
2017-10-23 15:15:27 -0400

    Applied enhanced loop and created a loop that would allow the user to enter as many colors as they want.

--- src/me/aoa4eva/Main.java
+++ src/me/aoa4eva/Main.java
@@ -14,7 +14,7 @@ public class Main {
          3. Write a method to print all the colours in the array list
          */
 
-        int option=0;
+        int option = 0;
         Scanner keyboard = new Scanner(System.in);
         ArrayList <String> colours = new ArrayList<String>();
         String enterColour="";
@@ -24,27 +24,28 @@ public class Main {
         colours.add("green");
         colours.add("mauve");
 
-        System.out.println("1. Add a colour");
-        System.out.println("2. Delete a colour ");
-        System.out.println("3. Show the colour list");
-        System.out.println("4. Quit");
-        System.out.println("Enter a number to select a menu option:");
-        option = keyboard.nextInt();
-        keyboard.nextLine();
+        do{
+            System.out.println("1. Add a colour");
+            System.out.println("2. Delete a colour ");
+            System.out.println("3. Show the colour list");
+            System.out.println("4. Quit");
+            System.out.println("Enter a number to select a menu option:");
+            option = keyboard.nextInt();
+            keyboard.nextLine();
 
-        if(option==1)
-        {
-            System.out.println("Enter the colour to add to the arraylist of colours");
-            enterColour=keyboard.nextLine();
-            colours.add(enterColour);
-            System.out.println(colours.get(colours.indexOf(enterColour)));
+                if (option == 1) {
+                    System.out.println("Enter the colour to add to the arraylist of colours");
+                    enterColour = keyboard.nextLine();
+                    colours.add(enterColour);
+                    System.out.println(colours.get(colours.indexOf(enterColour)));
 
-        }
+                }
+            }while(option == 1);
 
         if(option==2)
         {
             System.out.println("Enter the colour to delete from the arraylist of colours:");
-            enterColour=keyboard.nextLine().toLowerCase();;
+            enterColour=keyboard.nextLine().toLowerCase();
             if(colours.contains(enterColour))
                 colours.remove(enterColour);
             else
@@ -55,9 +56,9 @@ public class Main {
 
         if(option==3)
         {
-            for(int counter=0; counter<colours.size(); counter++)
+            for(String colour: colours)
             {
-                System.out.println(colours.get(counter));
+                System.out.println(colours.get(colours.indexOf(colour)));
             }
         }
 
@@ -66,4 +67,5 @@ public class Main {
 
     }
 
-}
+    }
+
